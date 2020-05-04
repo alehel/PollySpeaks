5cd3918ad85a10fdcb047960f94430d168aada72
Aleksander Helgaker <aleksander.helgaker@gmail.com>
2020-05-04 16:03:49 +0200

    Add initial Unit tests.

---
+++ src/test/java/io/aleksander/utils/StringResourceTest.java
@@ -0,0 +1,22 @@
+package io.aleksander.utils;
+
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.MissingResourceException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringResourceTest {
+
+  @Test
+  void getString_validPropertyKey() {
+    String testString = StringResource.getString("testString");
+    Assert.assertEquals(testString, "Test String");
+  }
+
+  @Test
+  void getString_invalidPropertyKey() {
+    assertThrows(MissingResourceException.class, () -> StringResource.getString("invalidPropertyKey"));
+  }
+}
