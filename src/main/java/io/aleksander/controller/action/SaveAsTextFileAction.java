3ecf60c5c09615b5229fc9296e2dcc56f6de0c8e
Aleksander Helgaker <aleksander.helgaker@gmail.com>
2020-05-07 07:47:18 +0200

    Don't show "Save As..." if the file has already been saved once before, show file name in title bar and add support for "New File".

---
+++ src/main/java/io/aleksander/controller/action/SaveAsTextFileAction.java
@@ -0,0 +1,61 @@
+package io.aleksander.controller.action;
+
+import io.aleksander.model.DocumentMetadata;
+import io.aleksander.utils.FileHandler;
+import io.aleksander.utils.StringResource;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import static io.aleksander.utils.StringResource.OVERWRITE_FILE;
+import static io.aleksander.utils.StringResource.WARNING;
+
+public class SaveAsTextFileAction implements ActionListener {
+
+  private final Component parent;
+  private final JTextArea textArea;
+  private final DocumentMetadata documentMetadata;
+
+  public SaveAsTextFileAction(Component parent, JTextArea textArea, DocumentMetadata documentMetadata) {
+    this.parent = parent;
+    this.textArea = textArea;
+    this.documentMetadata = documentMetadata;
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    JFileChooser fileChooser = new JFileChooser();
+    if(fileChooser.showSaveDialog(parent) == JFileChooser.APPROVE_OPTION) {
+      try {
+        String text = textArea.getText();
+        File selectedFile = fileChooser.getSelectedFile();
+
+        if(selectedFile.exists()) {
+          int userChoice = JOptionPane.showConfirmDialog(
+              parent,
+              StringResource.getString(OVERWRITE_FILE),
+              StringResource.getString(WARNING),
+              JOptionPane.YES_NO_OPTION,
+              JOptionPane.WARNING_MESSAGE);
+
+          if(userChoice != JOptionPane.YES_OPTION) {
+            return;
+          }
+        }
+
+        FileHandler.writeTextToFile(text, selectedFile.getAbsolutePath());
+        documentMetadata.setTextIsAltered(false);
+        documentMetadata.setDocumentPath(selectedFile.getAbsolutePath());
+        documentMetadata.setDocumentName(selectedFile.getName());
+      } catch (IOException exception) {
+        System.out.println(exception);
+      }
+    }
+  }
+}
--- src/main/java/io/aleksander/controller/action/SaveTextFileAction.java
+++ src/main/java/io/aleksander/controller/action/SaveTextFileAction.java
@@ -1,52 +1,39 @@
 package io.aleksander.controller.action;
 
+import io.aleksander.gui.MainFrame;
+import io.aleksander.model.DocumentMetadata;
 import io.aleksander.utils.FileHandler;
-import io.aleksander.utils.StringResource;
 
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
-import static io.aleksander.utils.StringResource.OVERWRITE_FILE;
-import static io.aleksander.utils.StringResource.WARNING;
-
 public class SaveTextFileAction implements ActionListener {
-
-  private final Component parent;
   private final JTextArea textArea;
+  private final MainFrame view;
+  private final DocumentMetadata documentMetadata;
 
-  public SaveTextFileAction(Component parent, JTextArea textArea) {
-    this.parent = parent;
+  public SaveTextFileAction(MainFrame view, JTextArea textArea, DocumentMetadata documentMetadata) {
+    this.view = view;
     this.textArea = textArea;
+    this.documentMetadata = documentMetadata;
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    JFileChooser fileChooser = new JFileChooser();
-    if(fileChooser.showSaveDialog(parent) == JFileChooser.APPROVE_OPTION) {
+    // if the file hasn't been saved before, show the "Save As..." dialog.
+    if (documentMetadata.getDocumentPath() == null) {
+      SaveAsTextFileAction saveAsTextFileAction =
+          new SaveAsTextFileAction(view, textArea, documentMetadata);
+      saveAsTextFileAction.actionPerformed(e);
+    } else {
+      String text = textArea.getText();
+      File file = new File(documentMetadata.getDocumentPath());
       try {
-        String text = textArea.getText();
-        File selectedFile = fileChooser.getSelectedFile();
-
-        if(selectedFile.exists()) {
-          int userChoice = JOptionPane.showConfirmDialog(
-              parent,
-              StringResource.getString(OVERWRITE_FILE),
-              StringResource.getString(WARNING),
-              JOptionPane.YES_NO_OPTION,
-              JOptionPane.WARNING_MESSAGE);
-
-          if(userChoice != JOptionPane.YES_OPTION) {
-            return;
-          }
-        }
-
-        FileHandler.writeTextToFile(text, selectedFile.getAbsolutePath());
+        FileHandler.writeTextToFile(text, file.getAbsolutePath());
+        documentMetadata.setTextIsAltered(false);
       } catch (IOException exception) {
         System.out.println(exception);
       }
